4b52e2ddda2c8da8ebf5a3232ffdaf9b29e42c43
TheSmager <thesmager@gmail.com>
2024-06-17 23:49:11 +0200

    preload added + minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,19 @@ import Preloader from '@/components/layout/Preloader';
 
 const App = (): ReactElement => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  // const lenis = new Lenis()
+  const lenis = new Lenis()
   
-  // function raf(time: number) {
-  //   lenis.raf(time)
-  //   requestAnimationFrame(raf)
-  // }
+  function raf(time: number) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
   
-  // requestAnimationFrame(raf)
+  requestAnimationFrame(raf)
 
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
-    }, 3000);
+    }, 3500);
   }, []);
 
   return isLoading ? (
